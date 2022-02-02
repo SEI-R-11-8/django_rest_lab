290d21faf721eb071066588455be393a91c42109
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-02 14:42:03 -0800

    added .env and .gitignore

--- mlb_api/mlb_django/settings.py
+++ mlb_api/mlb_django/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wtk&4#(fg=g2_f5!zq@30lwpif@i%$9prj^#ria+0gw^r5d-0c'
+from decouple import config
 
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
