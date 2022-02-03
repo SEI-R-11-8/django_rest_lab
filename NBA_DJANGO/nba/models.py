46523d504318558d599628c40996e55b8d910700
vladkim91 <vkcoding21@gmail.com>
2022-02-02 16:29:24 -0800

    models created

--- NBA_DJANGO/nba/models.py
+++ NBA_DJANGO/nba/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Conference(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    conference = models.ForeignKey(
+        Conference, on_delete=models.CASCADE, related_name='teams')
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    championships = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Player(models.Model):
+    team = models.ForeignKey(
+      Team, on_delete=models.CASCADE, related_name='players'
+    )
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=40)
+    age = models.IntegerField()
+    height = models.CharField(max_length=10)
+    image_url = models.TextField()
+    all_star_selections = models.IntegerField(default=0)
+
+    def __str__(self):
+      return self.name
