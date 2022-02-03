ab1c63f0b49f8efb394c72fef31e1cbf37b17529
vladkim91 <vkcoding21@gmail.com>
2022-02-02 16:59:06 -0800

    superuser created

--- NBA_DJANGO/nba/admin.py
+++ NBA_DJANGO/nba/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Conference, Team, Player
+
+admin.site.register(Conference)
+admin.site.register(Team)
+admin.site.register(Player)
