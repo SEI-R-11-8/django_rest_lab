1a1170f274aeb3d4ddbbb684e382bc6986bba4b2
vladkim91 <vkcoding21@gmail.com>
2022-02-03 05:55:52 -0800

    mvp

--- NBA_DJANGO/nba/serializers.py
+++ NBA_DJANGO/nba/serializers.py
@@ -1,12 +1,40 @@
 from rest_framework import serializers
 from .models import Conference, Team, Player
 
+
 class ConferenceSerializer(serializers.HyperlinkedModelSerializer):
     teams = serializers.HyperlinkedRelatedField(
-        view_name='teams',
+        view_name='team_detail',
         many=True,
         read_only=True
     )
+
     class Meta:
-       model = Artist 
-       fields = ('id', 'name', 'teams' )
+        model = Conference
+        fields = ('id', 'name', 'teams')
+
+
+class TeamSerializer(serializers.HyperlinkedModelSerializer):
+    players = serializers.HyperlinkedRelatedField(
+        view_name='player_detail',
+        read_only=True,
+        many=True
+
+    )
+
+    class Meta:
+        model = Team
+        fields = ('id', 'name', 'players', 'city',
+                  'championships')
+
+
+class PlayerSerializer(serializers.HyperlinkedModelSerializer):
+    team = serializers.HyperlinkedRelatedField(
+        view_name='team_detail',
+        read_only=True
+    )
+
+    class Meta:
+        model = Player
+        fields = ('id', 'name', 'position',
+                  'age', 'height', 'image_url', 'all_star_selections' ,'team')
--- NBA_DJANGO/nba/views.py
+++ NBA_DJANGO/nba/views.py
@@ -2,25 +2,31 @@ from rest_framework import generics
 from .serializers import ConferenceSerializer, TeamSerializer, PlayerSerializer
 from .models import Conference, Team, Player
 
+
 class ConferenceList(generics.ListCreateAPIView):
     queryset = Conference.objects.all()
     serializer_class = ConferenceSerializer
+
 
 class ConferenceDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Conference.objects.all()
     serializer_class = ConferenceSerializer
 
+
 class TeamList(generics.ListCreateAPIView):
-    queryset = Team.objects.all()
+    queryset = Team.objects.all() 
     serializer_class = TeamSerializer
+
 
 class TeamDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
+
 class PlayerList(generics.ListCreateAPIView):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
+
 
 class PlayerDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Player.objects.all()
