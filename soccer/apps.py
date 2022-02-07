d6b1c4f1a19fdc43c04f2188b6f9769885c0325e
Joseph Venable <JJVenable@gmail.com>
2022-02-06 22:46:35 -0500

    players added

--- soccer/apps.py
+++ soccer/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class SoccerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'soccer'
--- soccer/models.py
+++ soccer/models.py
@@ -6,7 +6,12 @@ class Team(models.Model):
     established = models.CharField(max_length=50)
     number_of_players = models.TextField()
     
-
-
     def __str__(self):
       return self.name
+class Player(models.Model):
+  team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='players')
+  name = models.CharField(max_length=50)
+  age = models.CharField(max_length=10, default="Not Given")
+
+  def __str__(self):
+    return self.name
