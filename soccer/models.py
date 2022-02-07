30a15d7721b5c21a496d474599698a9e04dba2e2
Joseph Venable <JJVenable@gmail.com>
2022-02-06 21:34:16 -0500

    same error as class lesson, not app not added in installed_apps   UGH

--- soccer/models.py
+++ soccer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+    established = models.CharField(max_length=50)
+    number_of_players = models.TextField()
+    
+
+
+    def __str__(self):
+      return self.name
